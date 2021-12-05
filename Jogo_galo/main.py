0f037ce5f738515950e4b4d135cd384cd0cc6dda
adcpereira <95571213+adcpereira@users.noreply.github.com>
2021-12-05 17:08:20 +0000

    Add files via upload

---
+++ Jogo_galo/main.py
@@ -0,0 +1,15 @@
+def show_board(bd):
+    print("-------")
+    for i in range(3):
+        print("-------")
+        print(f"{bd[3 * i]} | {3 * i + 1} | {3 * i + 1}")
+
+
+
+def main():
+
+
+
+    pass
+
+main()
